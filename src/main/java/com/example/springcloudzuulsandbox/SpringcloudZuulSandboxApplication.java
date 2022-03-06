ec247794f0a1339916b8b2c691b9dbad254f7a5b
YoshikazuHosono <yyhhwork@gmail.com>
2022-03-06 09:50:08 +0900

    add

---
+++ src/main/java/com/example/springcloudzuulsandbox/SpringcloudZuulSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudzuulsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudZuulSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudZuulSandboxApplication.class, args);
+	}
+
+}
